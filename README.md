1ce4a0d0bdf62197d89c2c6a4523936b21b3a3f7
Muhammad Tehami <t3hami@gmail.com>
2018-08-19 15:04:27 +0500

    Test cases completed.

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
-# The Ultimate To-Do App Project:
+# The Ultimate To-Do App Project
 
 The Path to becoming the Ultimate Developer
 
-## Step 1A:
+## **Step 1A:**
 
 ### Todo RESTful API
 
 A TDD Approach to Building an API using NoSQL Database
+
+## Test cases:
+
+- For /todo/api/v1.0/tasks:
+1. Request method checking (For GET and POST)
+2. Checking missing data in PUT request
+
+- For /todo/api/v1.0/tasks/task_id
+1. Request method checking (For GET, PUT and DELETE)
+2. Checking whether provided task_id exist and valid.
+3. Checking missing data in PUT request
--- app.py
+++ app.py
@@ -27,14 +27,16 @@ def tasks():
         return jsonify(serialize_tasks)
 
     elif request.method == 'POST':
-        task = {
-            'title': request.json['title'],
-            'description': request.json['description'],
-            'done': False
-        }
-        mongo.db.tasks.insert(task)
-        return jsonify({'status': 'Success'})
-
+        try:
+            task = {
+                'title': request.json['title'],
+                'description': request.json['description'],
+                'done': False
+            }
+            id = mongo.db.tasks.insert(task)
+            return jsonify({'status': 'Success', '_id': str(id)})
+        except KeyError:
+            return jsonify({'status': 'Missing title or discription, or both.'})
     else:
         pass
 
@@ -42,31 +44,62 @@ def tasks():
 @app.route('/todo/api/v1.0/tasks/<task_id>', methods=['GET', 'PUT', 'DELETE'])
 def task(task_id):
     if request.method == 'GET':
-        task_cursor = mongo.db.tasks.find_one({'_id': ObjectId(task_id)})
-        if task_cursor:
-            task = {
-                '_id': str(task_cursor['_id']),
-                'title': task_cursor['title'],
-                'description': task_cursor['description'],
-                'done': task_cursor['done']
-            }
-            return jsonify(task)
+        if is_task_present(task_id):
+            task_cursor = mongo.db.tasks.find_one({'_id': ObjectId(task_id)})
+            if task_cursor:
+                task = {
+                    '_id': str(task_cursor['_id']),
+                    'title': task_cursor['title'],
+                    'description': task_cursor['description'],
+                    'done': task_cursor['done']
+                }
+                return jsonify(task)
+        else:
+            return jsonify({'status': 'There is no task with _id: ' + task_id})
 
     elif request.method == 'PUT':
-        mongo.db.tasks.save({
-            '_id': ObjectId(task_id),
-            'title': request.json['title'],
-            'description': request.json['description'],
-            'done': request.json['done']
-            })
-        return jsonify({'status': 'Success'})
+        if is_task_present(task_id):
+            if not request.json:
+                return jsonify({'status': 'Please provide atleast one key, value to update'})
+            old_task = {}
+            task_cursor = mongo.db.tasks.find_one({'_id': ObjectId(task_id)})
+            if task_cursor:
+                old_task = {
+                    '_id': ObjectId(task_id),
+                    'title': task_cursor['title'],
+                    'description': task_cursor['description'],
+                    'done': task_cursor['done']
+                }
+            updated_task = {}
+            for key in old_task.keys():
+                try:
+                    updated_task[key] = request.json[key]
+                except:
+                    updated_task[key] = old_task[key]
+            mongo.db.tasks.save(updated_task)
+            return jsonify({'status': 'Success'})
+        else:
+            return jsonify({'status': 'There is no task with _id: ' + task_id})
 
     elif request.method == 'DELETE':
-        mongo.db.tasks.remove({'_id': ObjectId(task_id)})
-        return jsonify({'status': 'Success'})
+        if is_task_present(task_id):
+            mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+            return jsonify({'status': 'Success'})
+        else:
+            return jsonify({'status': 'There is no task with _id: ' + task_id})
     else:
         pass
 
+
+def is_task_present(task_id):
+    try:
+        task_cursor = mongo.db.tasks.find_one({'_id': ObjectId(task_id)})
+    except:
+        return False
+    if task_cursor:
+        return True
+    else:
+        False
 
 if __name__ == '__main__':
     app.run(debug=True)
